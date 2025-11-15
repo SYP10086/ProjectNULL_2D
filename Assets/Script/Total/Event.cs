e57837660da963b4e3c495ac340d3c8deb8e3e73
Healthy brother <153404819+SYP10086@users.noreply.github.com>
2025-11-15 18:04:40 +0800

    Revert "syp222"

--- Assets/Script/Total/Event.cs
+++ Assets/Script/Total/Event.cs
@@ -3,16 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public delegate void MyDel();
-public delegate void Myfloat(float a);
-public delegate void MyStrFloat(string a,float b);
 public static class Event
 {
     public static MyDel CleanSpeed;
     public static MyDel SpeedRe;
-    public static MyDel Init;
-    public static Myfloat Hit;
-    public static MyDel Death;
-    public static MyDel LocalChange;
-    public static MyStrFloat Attack;
-
 }
